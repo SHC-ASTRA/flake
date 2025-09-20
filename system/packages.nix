ff3a9731333859f1bf6fa49e579adcd6a76466dd
Roald Schaum <178021507+Roald-Schaum@users.noreply.github.com>
2025-09-20 14:20:02 -0500

    Update packages.nix

--- system/packages.nix
+++ system/packages.nix
@@ -1,17 +1,15 @@
 { pkgs, ... }: {
   environment.systemPackages = with pkgs; [
-    ffmpeg
     nixfmt
     nil
     gh
     socat
 
     git
-    pkgs.dotnetCorePackages.sdk_9_0-bin
+    # pkgs.dotnetCorePackages.sdk_9_0-bin
     ripgrep
     silver-searcher
     neovim
-    fastfetch
 
     # build stuff
     colcon
