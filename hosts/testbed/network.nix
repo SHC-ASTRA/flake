f7291274b8124a2893ee667077d2692d441c8d4e
ASTRA-SHC <astra@uah.edu>
2025-10-18 11:24:58 -0500

    Added network configuration to testbed for SSH

--- hosts/testbed/network.nix
+++ hosts/testbed/network.nix
@@ -3,16 +3,11 @@
   environment.systemPackages = [
     pkgs.hostapd
   ];
-  networking.interfaces.wlp4s0f3u1u4 = {
-    ipv4.addresses = [{
-      address = hosts.deck.ip;
-      prefixLength = 24;
-    }];
-  };
+  
   services = {
     hostapd = {
       enable = true;
-      radios.wlp4s0f3u1u4.networks.wlp4s0f3u1u4 = {
+      radios.wlp0s20fu6u4.networks.wlp0s20fu6u4 = {
         ssid = "testbed";
         apIsolate = false;
         authentication = {
--- system/network.nix
+++ system/network.nix
@@ -1,5 +1,7 @@
 { ... }:
 {
+  networking.networkmanager.enable = true;
+  networking.interaces.sixseven.macAddress = "74:FE:CE:35:29:38"
   services.openssh = {
     enable = true;
     settings = {
