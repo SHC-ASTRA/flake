580ca8a52be87a1fb3ced585227ab371b5085f81
Roald Schaum <178021507+Roald-Schaum@users.noreply.github.com>
2025-10-18 15:16:20 -0500

    Undo MAC set
    Kinda the opposite of what's desired

--- system/network.nix
+++ system/network.nix
@@ -3,7 +3,6 @@
   networking = {
     networkmanager.enable = true;
     firewall.enable = false;
-    interfaces.sixseven.macAddress = "74:FE:CE:35:29:38";
   };
   services.openssh = {
     enable = true;
