7935af0581a15dc2f602a6682882cf58ee250ca0
Roald Schaum <178021507+Roald-Schaum@users.noreply.github.com>
2025-10-18 15:15:30 -0500

    Un-spam networking

--- system/network.nix
+++ system/network.nix
@@ -1,8 +1,10 @@
 { ... }:
 {
-  networking.networkmanager.enable = true;
-  networking.firewall.enable = false;
-  networking.interfaces.sixseven.macAddress = "74:FE:CE:35:29:38";
+  networking = {
+    networkmanager.enable = true;
+    firewall.enable = false;
+    interfaces.sixseven.macAddress = "74:FE:CE:35:29:38";
+  };
   services.openssh = {
     enable = true;
     settings = {
