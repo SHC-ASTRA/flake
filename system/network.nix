15bdacc7fee9ee8583f896850ac1a6b819dedac4
ASTRA-SHC <astra@uah.edu>
2025-10-18 12:17:36 -0500

    disabled firewall (fixes ROS)

--- system/network.nix
+++ system/network.nix
@@ -1,7 +1,8 @@
 { ... }:
 {
   networking.networkmanager.enable = true;
-  networking.interaces.sixseven.macAddress = "74:FE:CE:35:29:38"
+  networking.firewall.enable = false;
+  networking.interfaces.sixseven.macAddress = "74:FE:CE:35:29:38";
   services.openssh = {
     enable = true;
     settings = {
